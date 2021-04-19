af3d6a4ef452cb215d8a506d6a7abfc6054b26ad
kreehal <kreehal@berkeley.edu>
2021-04-19 12:09:39 -0700

    improved modal & button colors

--- spotifyApp.js
+++ spotifyApp.js
@@ -76,7 +76,7 @@ function sidebar_close() {
 }
 
 
-
+// Get the modal
 var modal = document.getElementById("myModal");
 
 // Get the button that opens the modal
@@ -85,7 +85,7 @@ var btn = document.getElementById("myBtn");
 // Get the <span> element that closes the modal
 var span = document.getElementsByClassName("close")[0];
 
-// When the user clicks on the button, open the modal
+// When the user clicks the button, open the modal
 btn.onclick = function() {
   modal.style.display = "block";
 }

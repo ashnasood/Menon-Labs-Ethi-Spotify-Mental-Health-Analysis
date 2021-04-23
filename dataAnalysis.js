16c9447c722f361b69791f80d31448e10bc9fe32
shbgrwl <52204142+shbgrwl@users.noreply.github.com>
2021-04-22 22:18:22 -0400

    added documentation

--- dataAnalysis.js
+++ dataAnalysis.js
@@ -3,8 +3,31 @@ const readline = require('readline');
 
 const throwErrorCb = (err) => {if (err) throw err;};
 
+const emotions = ['Heartbroken', 'Upbeat', 'Romantic', 'Cheerful', 'Rebellious', 'Calm', 'Powerful', 'Carefree', 'Party Music', 'Reflective', 'Energized', 'Warm', 'Sentimental', 'Chill', 'Exuberant']
 
+/**
+ * @class ListeningSession utility object for all operations involving a session
+ *
+ * @field {number} size the number of songs in the session
+ * @field {Date} time the date and time that the final song ended
+ * @field {Array[Object]} songList the list of songs listened to during the session
+ *      each object represents a single song and has the following fields:
+ *          - endTime {Date}
+ *          - trackName {String}
+ *          - artistName {String}
+ *          - msPlayed {number}
+ *      array is sorted by endTime, with later times first
+ */
 class ListeningSession {
+    /**
+     * @param {Array[Object]} songs the list of songs to be part of the session
+     *      each object should represent a single song and have the following fields:
+     *          - endTime {Date}
+     *          - trackName {String}
+     *          - artistName {String}
+     *          - msPlayed {number}
+     *      array should be sorted by endTime, with later times first
+     */
     constructor(songs) {
         this.size = songs.length;
         this.time = songs[0].endTime;
@@ -12,6 +35,18 @@ class ListeningSession {
     }
 }
 
+/**
+ * Get all songs the user has listened to from their data
+ *
+ * @param {String} folderPath the path to the folder of downloaded data
+ * @return {Array[Object]} all songs in the streaming history
+ *      each object represents a single song and has the following fields:
+ *          - endTime {Date}
+ *          - trackName {String}
+ *          - artistName {String}
+ *          - msPlayed {number}
+ *      array is sorted by endTime, with later times first
+ */
 function getHistory(folderPath) {
     try {
         const files = fs.readdirSync(folderPath, throwErrorCb);
@@ -54,13 +89,31 @@ function getHistory(folderPath) {
     }
 }
 
-
+/**
+ * Divide straming history into listening sessions
+ *
+ * A listening session meets the following conditions:
+ *      - each track was played for between 30 seconds and 10 minutes, inclusive
+ *      - each pair of consecutive tracks was played within 30 minutes of each other, inclusive
+ *      - the session contains at least 10 songs meeting the above two criteria
+ *
+ * @param {Array[Object]} history all the songs a user has listened to
+ *      each object should represent a single song and have the following fields:
+ *          - endTime {Date}
+ *          - trackName {String}
+ *          - artistName {String}
+ *          - msPlayed {number}
+ *      array should be sorted by endTime, with later times first
+ *
+ * @return {Array[ListeningSession]} all the valid listening sessions from the user's data
+ *      array is sorted by endTime of final song in each session, with later times first
+ */
 function getListeningSessions(history) {
 
     // copy tracks with > 30s of time played into new array
     tracks = [];
     for (track of history) {
-        if (track.msPlayed > 30000)
+        if (track.msPlayed >= 30*1000 && track.msPlayed <= 10*60*1000)
             tracks.push(track);
     }
 
@@ -77,12 +130,17 @@ function getListeningSessions(history) {
     // create session objects for all groups with > 10 songs
     sessions = [];
     for (group of groups) {
-        if (group.length > 10) {
+        if (group.length >= 10) {
             sessions.push(new ListeningSession(group));
         }
     }
 
     return sessions;
+}
+
+
+function databaseInit() {
+    
 }
 
 history = getHistory(process.argv[2]);
